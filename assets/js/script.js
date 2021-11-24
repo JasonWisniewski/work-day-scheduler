3299f956436c56916a4d88a75c75d3cb5701de79
Jason Wisniewski <jwizinpa95@gmail.com>
2021-11-24 12:46:52 -0700

    tutor session, for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,30 @@
-console.log("js file is liinked correctly");
+var currentdateEl = document.getElementById("current-date");
+// display date on the top of the page
+currentdateEl.textContent= moment();
+
+var changeTaskColor = function() {
+
+  for (i=9; i<17; i++) {
+
+    var scheduleTime = document.getElementById(`${i}`);
+    // var scheduledTime = $(`#${i}).val()
+    console.log('time 1',scheduleTime.id);
+    if (moment().isAfter(scheduleTime.id)) {
+      $(`#${i}`).removeClass("bg-light");
+      $(`#${i}`).addClass("bg-danger");
+      console.log('time color',scheduleTime);
+    }
+    else
+    if (moment().format("hh") < parseInt(scheduleTime.id)) {
+      console.log(moment().format("hh"))
+      $(`#${i}`).removeClass("bg-light");
+      $(`#${i}`).addClass("bg-success");
+      console.log('time color2',scheduleTime);
+    }
+    // else(remove any old classes that might be on it)
+  }
+};
+
+// need to save tasks when refreshed.. either local storage or prevent clear
+
+changeTaskColor();
