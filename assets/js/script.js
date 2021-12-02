ed8f6944c8bee76ac87ee4e5c476365bf4b16b14
Jason Wisniewski <jwizinpa95@gmail.com>
2021-12-02 12:31:34 -0700

    cleaning comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,15 +42,6 @@ var changeTaskColor = function() {
       $(`#${i}`).addClass("past");
       console.log('time color2',scheduleTime);
     }
-    //  else
-    // this works below
-    // (moment().format("hh") < parseInt(scheduleTime.id))
-
-    //  if (moment().format("h") = parseInt(scheduleTime.id)){
-    //   $(`#${i}`).removeClass("bg-light");
-    //   $(`#${i}`).addClass("bg-warning");
-    //   console.log('time color2',scheduleTime);
-    //  }
   }
 };
 
@@ -61,23 +52,13 @@ var saveTask = function (){
   var text = $(this).siblings(".input-group").children(".textarea").val().trim();
   console.log('text-entered', text);
 
-  // !!grab time that was entered into, only grabs 9 right now!!!
   var time = $(this).siblings(".input-group").children(".textarea").attr('id');
   console.log('time',time);
 
   // consolelog what user input and that click registered
   console.log("click works");
-  
-  // and task time under tasks array out sourcing to another fxn??
-  // if (taskText && taskTime) {
-  //   createTask(taskText, taskTime, "tasks");
-  // }
 
   localStorage.setItem (time, text);
-
-  // replace textarea with new content
-  // $(this).replaceWith(taskP);
-  // not working for all classes with save-btn only for first 9am :(
 }
 
 $(".saveBtn").click(saveTask);
